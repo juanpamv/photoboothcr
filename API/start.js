8e4394b2236253acc2d9dff87dec553e9c58b188
juanpamv <juanpamv3@gmail.com>
2018-12-17 08:35:05 -0600

    update

--- API/start.js
+++ API/start.js
@@ -16,7 +16,7 @@ require("./models/User");
 
 // Start our app!
 const app = require("./app");
-app.set("port", process.env.PORT || 80);
+app.set("port", process.env.PORT || 3001);
 const server = app.listen(app.get("port"), () => {
     console.log(`Express running → PORT ${server.address().port}`);
 });
