e7b1b03b779f7a62522c2e67a18b8c2c7c3f84a8
juanpamv <juanpamv3@gmail.com>
2018-12-17 21:43:03 -0600

    Update webpackDevServer.config.js

--- Photobooth/config/webpackDevServer.config.js
+++ Photobooth/config/webpackDevServer.config.js
@@ -50,13 +50,13 @@ module.exports = function(proxy, allowedHost) {
     // use an image, put it in `src` and `import` it from JavaScript instead.
     contentBase: paths.appPublic,
     // By default files from `contentBase` will not trigger a page reload.
-    watchContentBase: true,
+    //watchContentBase: true,
     // Enable hot reloading server. It will provide /sockjs-node/ endpoint
     // for the WebpackDevServer client so it can learn when the files were
     // updated. The WebpackDevServer client is included as an entry point
     // in the Webpack development configuration. Note that only changes
     // to CSS are currently hot reloaded. JS changes will refresh the browser.
-    hot: true,
+    //hot: true,
     // It is important to tell WebpackDevServer to use the same "root" path
     // as we specified in the config. In development, we always serve from /.
     publicPath: config.output.publicPath,
